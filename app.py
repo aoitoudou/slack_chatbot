06ce661460cf3d1b14903d79939275287da3a7b2
yasuwooo <kokita.yasuhiro.21@shizuoka.ac.jp>
2024-09-04 17:18:55 +0900

    I'm making qualify reaction component

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import re
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # ボットトークンを渡してアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -23,6 +27,35 @@ def message_hello(message, say):
         ],
         text=f"こんにちは、<@{message['user']}> さん！",
     )
+    
+# 資格取得のモチベを保つためのコンポーネントの作成
+@app.message(re.compile("資格|炎上|実力不足|足りない|まだまだ|もっと|ダメ"))
+def get_qualified(message, say):
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"<@{message['user']}>よ、自己研鑽、足りてるかい？"},
+            },
+            {
+                "type":"actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "はい"},
+                        "action_id": "qualified_yes_btn"
+                    },
+                    {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "いいえ"},
+                        "action_id": "qualified_no_btn"
+                    },
+                    
+                ]
+            }
+        ],
+        text=f"<@{message['user']}>よ、自己研鑽、足りてるかい？",
+    )
 
 @app.action("button_click")
 def action_button_click(body, ack, say):
@@ -30,6 +63,19 @@ def action_button_click(body, ack, say):
     ack()
     # チャンネルにメッセージを投稿します
     say(f"<@{body['user']['id']}> さんがボタンをクリックしました！")
+
+# "はい" ボタンのアクション
+@app.action("qualified_yes_btn")
+def action_yes_button_click(body, ack, say):
+    ack()
+    # say(f"<@{body['user']['id']}>、セーフ")
+    
+
+# "いいえ" ボタンのアクション
+@app.action("qualified_no_btn")
+def action_no_button_click(body, ack, say):
+    ack()
+    say(f"<@{body['user']['id']}> 、アウト")
 
 if __name__ == "__main__":
     # アプリを起動して、ソケットモードで Slack に接続します
